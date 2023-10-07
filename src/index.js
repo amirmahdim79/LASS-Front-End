4d6281efbbefd1549f5f7266dd04c5c68fb02a23
amir mahdi <mohamadian.am79@ut.ac.ir>
2023-10-07 18:54:01 +0330

    add sass and dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'styles/index.css';
+import 'styles/index.scss';
+import 'styles/colors.scss';
+import 'styles/mixins.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
